6e934da43f6e7aed140794ce290abf3445a8b796
cedric43-2718 <cedric43@protonmail.com>
2026-04-16 20:21:32 -0400

    refactoring reading list css v2

--- src/views/Project_Reading.vue
+++ src/views/Project_Reading.vue
@@ -1,15 +1,15 @@
 <template>
 	<main class="reading-layout">
 		<section class="article-section">
-			<div class="article-color" :class="{'section-hilight': isSec1}"></div>
+			<div class="article-color" data-background="bg-science" :class="{'section-hilight': isSec}"></div>
 			<div class="article-subject">
 				<p class="subject-title">Science</p>
 				<span>&</span>
 				<p class="subject-title">Computation</p>
 			</div>
 			<div class="article-grid"
-				@mouseenter="handleSection1Color"
-				@mouseleave="handleSection1Color"
+				@mouseenter="handleSectionColor"
+				@mouseleave="handleSectionColor"
 			>
 				<ul class="articles-list">
 					<li class="article-title">A Mathematical Theory of Computation
@@ -24,7 +24,6 @@
 							<li>1929</li>
 						</ul>
 					</li>
-			
 				</ul>
 			</div>
 		</section>
@@ -35,25 +34,10 @@
 
 import { ref } from 'vue'
 
-const isSec1 = ref(false)
-const isSec2 = ref(false)
-const isSec3 = ref(false)
-const isSec4 = ref(false)
+const isSec = ref(false)
 
-const handleSection1Color = () => {
-	isSec1.value = !isSec1.value
-}
-
-const handleSection2Color = () => {
-	isSec2.value = !isSec2.value
-}
-
-const handleSection3Color = () => {
-	isSec3.value = !isSec3.value
-}
-
-const handleSection4Color = () => {
-	isSec4.value = !isSec4.value
+const handleSectionColor = () => {
+	isSec.value = !isSec.value
 }
 
 </script>
@@ -68,12 +52,13 @@ const handleSection4Color = () => {
 	margin-bottom: var(--space-m-l);
 
 	.article-section{
+		grid-column: span 3;
 		display: grid;
 		grid-template-columns: subgrid;
+		grid-template-rows: max-content;
 
 		.article-color{
 			grid-column: 1 / 2;
-			background-color: var(--deep-forest);
 			transition: background-color .5s ease-in-out;
 			border-radius: 0 8px 8px 0;
 		}
@@ -92,78 +77,42 @@ const handleSection4Color = () => {
 			display: grid;
 			grid-template-columns: 1rem repeat(var(--col-count), 1fr) 1rem;
 			gap: 1rem;
-			padding-block: 1rem;
+			/* padding-block: 1rem; */
+
+			> *{
+				grid-column: 2 / span 2;
+				display: flex;
+				flex-direction: column;
+				row-gap: 1rem;
+			}
+
+			.articles-list > li{
+				font-size: var(--fs-note);
+			}
+
+			.article-info li {
+				font-size: var(--fs-small-note);
+				list-style: circle;
+				line-height: 1.5;
+			}
+
+			.article-info > li::marker{
+				color: var(--carrot-4);
+				font-size: 20px;
+			}
 		}
 	}
 }
 
-/* .subject{
-	text-align: center;
-	margin-top: 2rem;
-} */
-
-/* .articles-grid{
-	margin-top: 1rem;
-} */
-
-
-/* .articles-grid{
-	--col-count: 2;
-	display: grid;
-	grid-template-columns: 1rem repeat(var(--col-count), 1fr) 1rem;
-	gap: 1rem;
-	padding-block: 1rem;
-} */
-
-.articles-grid > *{
-	grid-column: 2 / span 2;
-	display: flex;
-	flex-direction: column;
-	row-gap: 1rem;
+[data-background="bg-science"]{
+	background-color: var(--lemon-burst-2);
 }
-
-.articles-list > li{
-	font-size: var(--fs-note);
-}
-
-.article-info li {
-	font-size: var(--fs-small-note);
-	list-style: circle;
-	line-height: 1.5;
-}
-
-.article-info > li::marker{
-	color: var(--carrot-4);
-	font-size: 20px;
-}
-
-/* .s-1-color{
-	grid-row: 1;
-	grid-column: 1 / 2;
-	margin-top: 2rem;
-	background-color: var(--deep-forest);
-	transition: background-color .5s ease-in-out;
-	border-radius: 0 8px 8px 0;
-} */
-
-/* .s-1{
-	grid-row: 1;
-	grid-column: 2 / 3;
-} */
-
-.ag-1{
-	grid-row: 1;
-	grid-column: 3 / 4;
-}
-
 
 
 .article-color.section-hilight{
 	background-color: var(--lime-3);
 	filter: saturate(1.5);
 }
-
-
 
 
 </style>
