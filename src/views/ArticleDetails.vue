be063e9261e12400d1e685f1ccb562ffbdd525ff
cedric43-2718 <cedric43@protonmail.com>
2026-04-20 20:07:47 -0400

    added styles to blog details page

---
+++ src/views/ArticleDetails.vue
@@ -0,0 +1,52 @@
+<template>
+
+	<main v-if="article" class="article-details">
+		<div class="details-container">
+			<h1>{{ article.title }}</h1>
+			<p>Author: {{ article.author }}, Published: {{ article.date }}</p>
+			<p>{{ article.description }}</p>
+		</div>
+	</main>
+	<!-- <div v-else>Loading...</div> -->
+</template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useGeneralStore } from '@/stores/appStore'
+
+const recipeStore = useGeneralStore()
+
+const article = ref(null)
+const id = ref("61f7ae57-bb72-45b0-a371-5dbf2470d939")
+
+onMounted(() => {
+	recipeStore.fetchRecipe(id.value)
+		.then((response) => {
+			console.log("response", response)
+			article.value = response
+		})
+		.catch((error) => {
+			console.log("error", error)
+		}) 
+})
+
+</script>
+
+
+<style scoped>
+
+.article-details{
+	display: grid;
+	place-content: center;
+	border: .5px solid var(--deepslate-1);
+
+	.details-container{
+		display: grid;
+		padding: 2rem;
+		max-inline-size: 800px;
+	}
+}
+
+</style>
