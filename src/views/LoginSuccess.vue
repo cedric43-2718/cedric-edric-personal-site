28d188f85e0e32e9ffaaa03bdf36b9db8e20a41b
cedric43-2718 <cedric43@protonmail.com>
2026-05-03 20:41:53 -0400

    direct endpoint auth functionality set up and working. production testing

--- src/views/LoginSuccess.vue
+++ src/views/LoginSuccess.vue
@@ -1,6 +1,7 @@
 <template>
 	<main class="login-success">
 		<h1>You are logged in</h1>
+		<p>This page is here for testing authentication. It's going to become a markdown editor with live preview that can be used to post articles and such to this site over the next few weeks or so.</p>
 		<a href="/.auth/logout?post_logout_redirect_uri=https://cedricedric.net">Logout</a>
 	</main>
 </template>
@@ -9,11 +10,16 @@
 
 main{
 	min-height: 100vh;
+	margin: auto;
 }
 
 .login-success{
 	display: grid;
-	place-content: center;
+	grid-template-columns: 1fr;
+	gap: 1rem;
+	align-content: center;
+	justify-items: center;
+	max-width: 50rem;
 
 	h1{
 		margin: auto;
