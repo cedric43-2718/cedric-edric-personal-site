57023cc488572c93afebf43c4539e8a6933cfe03
cedric43-2718 <cedric43@protonmail.com>
2026-05-19 20:04:08 -0400

    refactoring uplaod to storage

--- api/src/functions/uploadMkdToStorage.js
+++ api/src/functions/uploadMkdToStorage.js
@@ -6,22 +6,44 @@ app.http('uploadMkdToStorage', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
     
-        // const storageConnection = process.env.BLOB_STORAGE_CONNECTION_STRING
-        const storageConnection = process.env.DEV_BLOB_STORAGE_CONNECTION_STRING
+        // set connection properties
+        const storageConnection = process.env.DEV_BLOB_STORAGE_CONNECTION_STRING || process.env.BLOB_STORAGE_CONNECTION_STRING
         const containerName = 'markdown-files'
         
-    // recieve data from frontend
-    
-        // const body = await request.json()
-        const body = await JSON.parse(request)
-        const markdownContent = body.content
-        const markdownMeta = body.metaData
-        const fileName = body.fileName
+        // parse incoming request depending payload 
+        const parseRequest = async (req) => {
+            if (!req) {
+                throw new Error('the request is empty')
+            }
+            try {
+                // if the request already has a body
+                if(req.body) return req.body
+                // if the request is raw text
+                if(typeof req === 'text' || typeof req === 'string') return JSON.parse(req)
+            } catch(err) {
+                context.log('error parsing request', err)
+            }
+        }
+
+        const body = await parseRequest(request)
+
+        if(!body) {
+          return { status: 400, body: 'Request body not found or could not be parsed as JSON' }  
+        }
+        
+        // destructure the request into key variables expected by blob container
+        const markdownContent = body.content || body.markdown || ''
+        const markdownMeta = body.metaData || body.meta || {}
+        const fileName = body.fileName || `post-${Date.now()}.md`
         // const fileName = request.query.get('file')
 
         if(!markdownContent) {
-            return { status: 400, body: 'the request body did not contain the expected markdown content'}
+            return { status: 400, body: 'the request body did not contain markdown content'}
         }
+
+        // connect to blob storage
+
+        
 
     // setup connection to storage
 
